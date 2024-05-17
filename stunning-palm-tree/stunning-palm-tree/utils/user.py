5f327b260905590aa4859698aacec458313381f5
Causapin-PaulaSuezane <23-02644@g.batstate-u.edu.ph>
2024-05-17 11:50:01 +0800

    wieee

--- stunning-palm-tree/stunning-palm-tree/utils/user.py
+++
@@ -1,6 +0,0 @@
-class User:
-	 def __init__(self, username, password, points = 0, wins = 0):		
-			self.username = username
-			self.password = password
-			self.points = points
-			self.wins = wins
