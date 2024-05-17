539c3aec4ffcf7c5806cd3f1fea74e94c638ca9e
Causapin-PaulaSuezane <23-02644@g.batstate-u.edu.ph>
2024-05-17 12:00:53 +0800

    RENAMED FOLDER

---
+++ d final_lab_exam_cs121_CAUSAPIN_PAULA_CS1202/utils/score.py
@@ -0,0 +1,6 @@
+class Score:
+    def __init__(self, username, points = 0, wins = 0):
+        self.username = username
+        self.points = points
+        self.wins = wins
+        
--- stunning-palm-tree/utils/score.py
+++
@@ -1,6 +0,0 @@
-class Score:
-    def __init__(self, username, points = 0, wins = 0):
-        self.username = username
-        self.points = points
-        self.wins = wins
-        
